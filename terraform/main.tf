3dc6dd9ee9581fb637791e4efb6f7a93692a8c3c
Shuai Zhang <100640102+shuaizhang-rh@users.noreply.github.com>
2025-05-01 22:39:25 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,37 @@ resource "time_sleep" "wait_10_seconds" {
 resource "null_resource" "final" {
   depends_on = [time_sleep.wait_10_seconds]
 }
+
+# --- Parallel branch for dependency graph testing ---
+
+resource "null_resource" "parallel_start" {}
+
+resource "time_sleep" "wait_3_seconds_parallel" {
+  depends_on = [null_resource.parallel_start]
+  create_duration = "3s"
+}
+
+resource "null_resource" "parallel_end" {
+  depends_on = [time_sleep.wait_3_seconds_parallel]
+}
+
+# --- Count-based resources (creates 3 similar resources) ---
+
+resource "null_resource" "batch_step" {
+  count = 3
+  triggers = {
+    index = count.index
+  }
+}
+
+# --- Conditional execution with variable (optional future test) ---
+
+variable "enable_extra_step" {
+  type    = bool
+  default = false
+}
+
+resource "null_resource" "optional_step" {
+  count      = var.enable_extra_step ? 1 : 0
+  depends_on = [null_resource.final]
+}
